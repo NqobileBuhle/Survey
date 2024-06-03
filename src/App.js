720d86ca1c4af96ff85945f9fad16c30b98a0cc2
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-06-03 09:06:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import SurveyForm from './Components/SurveyForm';
+import ViewResults from './Components/ViewResults';
+import './index.css';
+
+const App = () => {
+    return (
+        <Router>
+          <div className="header">
+                <h1 className="header-title">Survey</h1>
+            </div>
+            <nav style={{ display: 'flex', justifyContent: 'flex-end', padding: '1em' }}>
+                <Link to="/filloutsurvey" style= {{ marginRight: '1em' }}>Fill Out Survey</Link>
+                <Link to="/viewsurveyresults" style={{ marginRight: '1em' }}>View Survey Results</Link>
+            </nav>
+
+            <Switch>
+                <Route path="/filloutsurvey" component={SurveyForm} />
+                <Route path="/viewsurveyresults" component={ViewResults} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
